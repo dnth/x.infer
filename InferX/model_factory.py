5f1dfc757cf1e7b0ac567638f5bf4708a325fa17
dnth <dickson.neoh@gmail.com>
2024-10-10 12:59:06 +0800

    add vlrmblip2 model

--- InferX/model_factory.py
+++ InferX/model_factory.py
@@ -1,9 +1,11 @@
-from .transformers.blip2 import BLIP2
+from .transformers.blip2 import BLIP2, VLRMBlip2
 
 
 def get_model(model_type: str, implementation: str, **kwargs):
     if implementation == "transformers":
         if model_type == "Salesforce/blip2-opt-2.7b":
             return BLIP2(model_name="Salesforce/blip2-opt-2.7b", **kwargs)
+        elif model_type == "sashakunitsyn/vlrm-blip2-opt-2.7b":
+            return VLRMBlip2(model_name="sashakunitsyn/vlrm-blip2-opt-2.7b", **kwargs)
     else:
         raise ValueError(f"Unsupported model type: {model_type}")
--- InferX/transformers/blip2.py
+++ InferX/transformers/blip2.py
@@ -44,3 +44,20 @@ class BLIP2(BaseModel):
 
     def postprocess(self, prediction):
         return self.processor.batch_decode(prediction, skip_special_tokens=True)[0]
+
+
+class VLRMBlip2(BLIP2):
+    def __init__(self, model_name: str, **kwargs):
+        super().__init__(model_name, **kwargs)
+        self.load_vlrm_weights()
+
+    def load_vlrm_weights(self):
+        from huggingface_hub import hf_hub_download
+
+        finetuned_weights_state_dict = torch.load(
+            hf_hub_download(
+                repo_id="sashakunitsyn/vlrm-blip2-opt-2.7b",
+                filename="vlrm-blip2-opt-2.7b.pt",
+            )
+        )
+        self.model.load_state_dict(finetuned_weights_state_dict, strict=False)
