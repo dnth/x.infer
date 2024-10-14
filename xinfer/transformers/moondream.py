b016a31b610ecf785176bccf8c5a904c257d1952
dnth <dickson.neoh@gmail.com>
2024-10-14 13:42:36 +0800

    update moondream for batch inference

--- xinfer/transformers/moondream.py
+++ xinfer/transformers/moondream.py
@@ -1,5 +1,8 @@
+import time
+
 import requests
 import torch
+from loguru import logger
 from PIL import Image
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
@@ -15,36 +18,67 @@ class Moondream(BaseModel):
         self,
         model_id: str = "vikhyatk/moondream2",
         revision: str = "2024-08-26",
+        device: str = "auto",
+        dtype: str = "float16",
         **kwargs,
     ):
-        self.model_id = model_id
+        device = "cuda" if device == "auto" and torch.cuda.is_available() else "cpu"
+
+        logger.info(f"Using device: {device}")
+        logger.info(f"Using dtype: {dtype}")
+
+        dtype_map = {
+            "float32": torch.float32,
+            "float16": torch.float16,
+            "bfloat16": torch.bfloat16,
+        }
+        if dtype not in dtype_map:
+            raise ValueError("dtype must be one of 'float32', 'float16', or 'bfloat16'")
+        dtype = dtype_map[dtype]
+
+        super().__init__(model_id, device, dtype)
         self.revision = revision
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.load_model(**kwargs)
+
+    def preprocess(
+        self,
+        images: str | list[str],
+    ):
+        if not isinstance(images, list):
+            images = [images]
+
+        processed_images = []
+        for image_path in images:
+            if not isinstance(image_path, str):
+                raise ValueError("Input must be a string (local path or URL)")
+
+            if image_path.startswith(("http://", "https://")):
+                image = Image.open(requests.get(image_path, stream=True).raw).convert(
+                    "RGB"
+                )
+            else:
+                # Assume it's a local path
+                try:
+                    image = Image.open(image_path).convert("RGB")
+                except FileNotFoundError:
+                    raise ValueError(f"Local file not found: {image_path}")
+
+            processed_images.append(image)
+
+        return processed_images
 
     def load_model(self, **kwargs):
         self.model = AutoModelForCausalLM.from_pretrained(
             self.model_id, trust_remote_code=True, revision=self.revision
-        )
-
-        if self.device == "cuda":
-            self.model = self.model.to(self.device, torch.bfloat16)
+        ).to(self.device, self.dtype)
 
         self.model = torch.compile(self.model, mode="max-autotune")
         self.model.eval()
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_id)
 
-    def infer(self, image: str, prompt: str = None, **generate_kwargs):
-        if isinstance(image, str):
-            if image.startswith(("http://", "https://")):
-                image = Image.open(requests.get(image, stream=True).raw).convert("RGB")
-            else:
-                raise ValueError("Input string must be an image URL for BLIP2")
-        else:
-            raise ValueError(
-                "Input must be either an image URL or a PIL Image for BLIP2"
-            )
-
+    def infer(self, image: str, prompt: str = None, verbose=False, **generate_kwargs):
+        start_time = time.perf_counter()
+        image = self.preprocess(image)
         encoded_image = self.model.encode_image(image)
         output = self.model.answer_question(
             question=prompt,
@@ -52,18 +86,28 @@ class Moondream(BaseModel):
             tokenizer=self.tokenizer,
             **generate_kwargs,
         )
-
+        end_time = time.perf_counter()
+        inference_time = end_time - start_time
+        if verbose:
+            logger.info(f"Inference time: {inference_time*1000:.4f} ms")
+            logger.info(f"Device: {self.device}")
+            logger.info(f"Dtype: {self.dtype}")
         return output
 
-    def infer_batch(self, images: list[str], prompts: list[str], **generate_kwargs):
-        images = [
-            Image.open(requests.get(image, stream=True).raw).convert("RGB")
-            for image in images
-        ]
+    def infer_batch(
+        self, images: list[str], prompts: list[str], verbose=False, **generate_kwargs
+    ):
+        start_time = time.perf_counter()
+        images = self.preprocess(images)
         prompts = [prompt for prompt in prompts]
 
         outputs = self.model.batch_answer(
             images, prompts, self.tokenizer, **generate_kwargs
         )
-
+        end_time = time.perf_counter()
+        inference_time = end_time - start_time
+        if verbose:
+            logger.info(f"Inference time: {inference_time*1000:.4f} ms")
+            logger.info(f"Device: {self.device}")
+            logger.info(f"Dtype: {self.dtype}")
         return outputs
