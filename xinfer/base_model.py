06463448c62307c72084a51ebcdbe6dbf1df30b0
Dickson Neoh <dickson.neoh@gmail.com>
2024-10-14 14:56:55 +0800

    Batch inference support (#10)
    * implement batch inference for moondream
    
    * rename functions to infer
    
    * add batch inference for automodels
    
    * add verbose to batch inference
    
    * Improve image handling in Vision2SeqModel
    
    - Update preprocess method to handle both local file paths and URLs
    - Add error handling for local file not found
    - Update type hints for infer and infer_batch methods in BaseModel
    - Fix typo in image file extension in notebook example
    
    * update auto class to let user choose device and dtype
    
    * update moondream for batch inference
    
    * default dtype to float32
    
    * let user specify revision
    
    * update logger information
    
    * update
    
    * batch inference for timm
    
    * update
    
    * udpate
    
    * cleanup
    
    * update name
    
    * update ultralytics api
    
    * update yolo nb

--- xinfer/base_model.py
+++ xinfer/base_model.py
@@ -2,13 +2,19 @@ from abc import ABC, abstractmethod
 
 
 class BaseModel(ABC):
-    def __init__(self, model_id: str, **kwargs):
+    def __init__(self, model_id: str, device: str, dtype: str):
         self.model_id = model_id
+        self.device = device
+        self.dtype = dtype
 
     @abstractmethod
-    def load_model(self, **kwargs):
+    def load_model(self):
         pass
 
     @abstractmethod
-    def inference(self, image, prompt):
+    def infer(self, image: str, prompt: str):
+        pass
+
+    @abstractmethod
+    def infer_batch(self, images: list[str], prompts: list[str]):
         pass
--- xinfer/transformers/moondream.py
+++ xinfer/transformers/moondream.py
@@ -1,5 +1,8 @@
+import time
+
 import requests
 import torch
+from loguru import logger
 from PIL import Image
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
@@ -13,38 +16,68 @@ from ..model_registry import ModelInputOutput, register_model
 class Moondream(BaseModel):
     def __init__(
         self,
-        model_name: str = "vikhyatk/moondream2",
+        model_id: str = "vikhyatk/moondream2",
         revision: str = "2024-08-26",
-        **kwargs,
+        device: str = "cpu",
+        dtype: str = "float32",
     ):
-        self.model_name = model_name
+        logger.info(f"Model: {model_id}")
+        logger.info(f"Revision: {revision}")
+        logger.info(f"Device: {device}")
+        logger.info(f"Dtype: {dtype}")
+
+        dtype_map = {
+            "float32": torch.float32,
+            "float16": torch.float16,
+            "bfloat16": torch.bfloat16,
+        }
+        if dtype not in dtype_map:
+            raise ValueError("dtype must be one of 'float32', 'float16', or 'bfloat16'")
+        dtype = dtype_map[dtype]
+
+        super().__init__(model_id, device, dtype)
         self.revision = revision
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.load_model(**kwargs)
+        self.load_model()
 
-    def load_model(self, **kwargs):
+    def preprocess(
+        self,
+        images: str | list[str],
+    ):
+        if not isinstance(images, list):
+            images = [images]
+
+        processed_images = []
+        for image_path in images:
+            if not isinstance(image_path, str):
+                raise ValueError("Input must be a string (local path or URL)")
+
+            if image_path.startswith(("http://", "https://")):
+                image = Image.open(requests.get(image_path, stream=True).raw).convert(
+                    "RGB"
+                )
+            else:
+                # Assume it's a local path
+                try:
+                    image = Image.open(image_path).convert("RGB")
+                except FileNotFoundError:
+                    raise ValueError(f"Local file not found: {image_path}")
+
+            processed_images.append(image)
+
+        return processed_images
+
+    def load_model(self):
         self.model = AutoModelForCausalLM.from_pretrained(
-            self.model_name, trust_remote_code=True, revision=self.revision
-        )
-
-        if self.device == "cuda":
-            self.model = self.model.to(self.device, torch.bfloat16)
+            self.model_id, trust_remote_code=True, revision=self.revision
+        ).to(self.device, self.dtype)
 
         self.model = torch.compile(self.model, mode="max-autotune")
         self.model.eval()
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_id)
 
-    def inference(self, image: str, prompt: str = None, **generate_kwargs):
-        if isinstance(image, str):
-            if image.startswith(("http://", "https://")):
-                image = Image.open(requests.get(image, stream=True).raw).convert("RGB")
-            else:
-                raise ValueError("Input string must be an image URL for BLIP2")
-        else:
-            raise ValueError(
-                "Input must be either an image URL or a PIL Image for BLIP2"
-            )
-
+    def infer(self, image: str, prompt: str = None, verbose=False, **generate_kwargs):
+        start_time = time.perf_counter()
+        image = self.preprocess(image)
         encoded_image = self.model.encode_image(image)
         output = self.model.answer_question(
             question=prompt,
@@ -52,5 +85,28 @@ class Moondream(BaseModel):
             tokenizer=self.tokenizer,
             **generate_kwargs,
         )
-
+        end_time = time.perf_counter()
+        inference_time = end_time - start_time
+        if verbose:
+            logger.info(f"Inference time: {inference_time*1000:.4f} ms")
+            logger.info(f"Device: {self.device}")
+            logger.info(f"Dtype: {self.dtype}")
         return output
+
+    def infer_batch(
+        self, images: list[str], prompts: list[str], verbose=False, **generate_kwargs
+    ):
+        start_time = time.perf_counter()
+        images = self.preprocess(images)
+        prompts = [prompt for prompt in prompts]
+
+        outputs = self.model.batch_answer(
+            images, prompts, self.tokenizer, **generate_kwargs
+        )
+        end_time = time.perf_counter()
+        inference_time = end_time - start_time
+        if verbose:
+            logger.info(f"Inference time: {inference_time*1000:.4f} ms")
+            logger.info(f"Device: {self.device}")
+            logger.info(f"Dtype: {self.dtype}")
+        return outputs
