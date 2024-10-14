7e571367dd35d4e6fbac0ddf34eb4dcf9cd48cf6
dnth <dickson.neoh@gmail.com>
2024-10-14 22:56:22 +0800

    Bump version: 0.0.2 → 0.0.3

--- xinfer/__init__.py
+++ xinfer/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Dickson Neoh"""
 __email__ = "dickson.neoh@gmail.com"
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 from .core import create_model, list_models
 from .timm import *
