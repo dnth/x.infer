f9a157a090375a4d135f3bf17ed22c27da9b02fa
dnth <dickson.neoh@gmail.com>
2024-10-15 10:54:01 +0800

    inital working version of soft imports

--- xinfer/__init__.py
+++ xinfer/__init__.py
@@ -4,7 +4,38 @@ __author__ = """Dickson Neoh"""
 __email__ = "dickson.neoh@gmail.com"
 __version__ = "0.0.3"
 
+import importlib
+
+from .base_model import BaseModel
 from .core import create_model, list_models
-from .timm import *
-from .transformers import *
-from .ultralytics import *
+from .model_registry import ModelInputOutput, register_model
+
+
+def soft_import(name: str):
+    try:
+        importlib.import_module(name)
+        return True
+    except ModuleNotFoundError as e:
+        if str(e) != f"No module named '{name}'":
+            raise e
+        return False
+
+
+timm_available = soft_import("timm")
+transformers_available = soft_import("transformers")
+ultralytics_available = soft_import("ultralytics")
+
+if timm_available:
+    from .timm import *
+if transformers_available:
+    from .transformers import *
+if ultralytics_available:
+    from .ultralytics import *
+
+__all__ = [
+    "create_model",
+    "list_models",
+    "register_model",
+    "BaseModel",
+    "ModelInputOutput",
+]
--- xinfer/core.py
+++ xinfer/core.py
@@ -1,10 +1,41 @@
+from __future__ import annotations
+
+import importlib
+
 from rich.console import Console
 from rich.table import Table
 
 from .model_registry import model_registry
-from .timm.timm_model import TimmModel
-from .transformers.auto import Vision2SeqModel
-from .ultralytics.ultralytics_model import UltralyticsModel
+
+
+def soft_import(name: str):
+    try:
+        importlib.import_module(name)
+        return True
+    except ModuleNotFoundError as e:
+        if str(e) != f"No module named '{name}'":
+            raise e
+        return False
+
+
+timm_available = soft_import("timm")
+transformers_available = soft_import("transformers")
+ultralytics_available = soft_import("ultralytics")
+
+if timm_available:
+    from .timm.timm_model import TimmModel
+else:
+    TimmModel = type("TimmModel", (), {})
+
+if transformers_available:
+    from .transformers.auto import Vision2SeqModel
+else:
+    Vision2SeqModel = type("Vision2SeqModel", (), {})
+
+if ultralytics_available:
+    from .ultralytics.ultralytics_model import UltralyticsModel
+else:
+    UltralyticsModel = type("UltralyticsModel", (), {})
 
 
 def create_model(model: str | TimmModel | Vision2SeqModel | UltralyticsModel, **kwargs):
