058eed727c9742a3a254928396cd7f34969cf3f1
Dickson Neoh <dickson.neoh@gmail.com>
2024-11-06 23:02:58 +0800

    Open browser to fastapi docs page when running model_serve (#54)
    * open browser
    
    * add serving test basic
    
    * add option to automatically open browser

--- xinfer/serve.py
+++ xinfer/serve.py
@@ -68,6 +68,7 @@ def serve_model(
     host: str = "127.0.0.1",
     port: int = 8000,
     blocking: bool = True,
+    open_api_docs: bool = True,
     **model_kwargs,
 ):
     deployment_kwargs = deployment_kwargs or {}
@@ -84,12 +85,26 @@ def serve_model(
     app = deployment.bind(model_id, **model_kwargs)
 
     try:
-        handle = serve.run(app, blocking=blocking)
-        if not blocking:
+        handle = serve.run(app)
+        logger.info(f"Open FastAPI docs at http://{host}:{port}/docs")
+        if open_api_docs:
+            import webbrowser
+
+            webbrowser.open(f"http://{host}:{port}/docs")
+
+        if blocking:
+            try:
+                while True:
+                    time.sleep(1)
+            except (KeyboardInterrupt, SystemExit):
+                logger.info("Receiving shutdown signal. Cleaning up...")
+                serve.shutdown()
+        else:
             logger.info(
                 "Running server in non-blocking mode, remember to call serve.shutdown() to stop the server"
             )
-            return handle  # Return handle without shutting down
-    except (KeyboardInterrupt, SystemExit):
-        logger.info("Receiving shutdown signal. Cleaning up...")
+            return handle
+    except Exception as e:
+        logger.error(f"Error starting server: {str(e)}")
         serve.shutdown()
+        raise
