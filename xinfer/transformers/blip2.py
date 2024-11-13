88e61813a889185ce96709b44503ecb729b9f9e2
Dickson Neoh <dickson.neoh@gmail.com>
2024-11-13 20:38:44 +0800

    New Model - BLIP2 Variant mBLIP and BLIP2 flan (#67)
    * add all remaining blip2 models from Salesforce
    
    * add multilingual blip

--- xinfer/transformers/blip2.py
+++ xinfer/transformers/blip2.py
@@ -17,6 +17,31 @@ from .vision2seq import Vision2SeqModel
     "transformers",
     ModelInputOutput.IMAGE_TEXT_TO_TEXT,
 )
+@register_model(
+    "Salesforce/blip2-flan-t5-xl",
+    "transformers",
+    ModelInputOutput.IMAGE_TEXT_TO_TEXT,
+)
+@register_model(
+    "Salesforce/blip2-flan-t5-xl-coco",
+    "transformers",
+    ModelInputOutput.IMAGE_TEXT_TO_TEXT,
+)
+@register_model(
+    "Salesforce/blip2-opt-2.7b-coco",
+    "transformers",
+    ModelInputOutput.IMAGE_TEXT_TO_TEXT,
+)
+@register_model(
+    "Gregor/mblip-mt0-xl",
+    "transformers",
+    ModelInputOutput.IMAGE_TEXT_TO_TEXT,
+)
+@register_model(
+    "Gregor/mblip-bloomz-7b",
+    "transformers",
+    ModelInputOutput.IMAGE_TEXT_TO_TEXT,
+)
 class BLIP2(Vision2SeqModel):
     def __init__(self, model_id: str, **kwargs):
         super().__init__(model_id, **kwargs)
