76442d7c93386794e600e1e33150ee05e202cc2b
dnth <dickson.neoh@gmail.com>
2024-11-13 17:02:37 +0800

    add all remaining blip2 models from Salesforce

--- xinfer/transformers/blip2.py
+++ xinfer/transformers/blip2.py
@@ -17,6 +17,21 @@ from .vision2seq import Vision2SeqModel
     "transformers",
     ModelInputOutput.IMAGE_TEXT_TO_TEXT,
 )
+@register_model(
+    "Salesforce/blip2-flan-t5-xl",
+    "transformers",
+    ModelInputOutput.IMAGE_TEXT_TO_TEXT,
+)
+@register_model(
+    "Salesforce/blip2-flan-t5-xl-coco",
+    "transformers",
+    ModelInputOutput.IMAGE_TEXT_TO_TEXT,
+)
+@register_model(
+    "Salesforce/blip2-opt-2.7b-coco",
+    "transformers",
+    ModelInputOutput.IMAGE_TEXT_TO_TEXT,
+)
 class BLIP2(Vision2SeqModel):
     def __init__(self, model_id: str, **kwargs):
         super().__init__(model_id, **kwargs)
