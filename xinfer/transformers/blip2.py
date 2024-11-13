c3702cff24229cf18deb84588c5fbaf9103153bd
dnth <dickson.neoh@gmail.com>
2024-11-13 17:28:08 +0800

    add multilingual blip

--- xinfer/transformers/blip2.py
+++ xinfer/transformers/blip2.py
@@ -32,6 +32,16 @@ from .vision2seq import Vision2SeqModel
     "transformers",
     ModelInputOutput.IMAGE_TEXT_TO_TEXT,
 )
+@register_model(
+    "Gregor/mblip-mt0-xl",
+    "transformers",
+    ModelInputOutput.IMAGE_TEXT_TO_TEXT,
+)
+@register_model(
+    "Gregor/mblip-bloomz-7b",
+    "transformers",
+    ModelInputOutput.IMAGE_TEXT_TO_TEXT,
+)
 class BLIP2(Vision2SeqModel):
     def __init__(self, model_id: str, **kwargs):
         super().__init__(model_id, **kwargs)
