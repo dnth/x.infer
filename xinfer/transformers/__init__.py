17bc408c9601eeae9ebf4e72fdac2f0e6f4ab492
dnth <dickson.neoh@gmail.com>
2024-11-19 11:59:19 +0800

    initial integration

--- xinfer/transformers/__init__.py
+++ xinfer/transformers/__init__.py
@@ -5,6 +5,7 @@ from .florence2 import Florence2
 from .joycaption import JoyCaption
 from .llama32 import Llama32Vision, Llama32VisionInstruct
 from .moondream import Moondream
+from .qwen2_vl import Qwen2VL
 from .vision2seq import Vision2SeqModel
 from .vlrm_blip2 import VLRMBlip2
 
